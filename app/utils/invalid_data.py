b49251a349389a217e14022fceaba2181564c26e
pooriat <pooria_taghdiri@hotmail.com>
2024-11-01 21:55:33 -0700

    feat:streaming the AI response

--- app/utils/invalid_data.py
+++ app/utils/invalid_data.py
@@ -2,11 +2,4 @@ import streamlit as st
 
 
 def invalid_stock_symbol():
-    st.markdown(
-        """
-        <p style="color:red;background-color:yellow">
-        Please enter a valid stock symbol to get the information.
-        </p>
-        """,
-        unsafe_allow_html=True,
-    )
+    st.warning("Please enter a valid stock symbol to get the information.", icon="⚠️")
