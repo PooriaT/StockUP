75e2397263eaba847a900e3b298c9835e523ab5c
Pooria Taghdiri <pooria@Poorias-MacBook-Air.local>
2025-01-02 19:42:12 -0800

    fix:pages directory name replaced by views

--- app/main.py
+++ app/main.py
@@ -1,26 +1,29 @@
 import streamlit as st
 
 if __name__ == "__main__":
+    pages_dir = "views"
     st.set_page_config(
         page_title="StockUP",
         page_icon="📈",
         layout="wide",
         initial_sidebar_state="expanded",
     )
-    home_page = st.Page("pages/home.py", title="Home", icon=":material/home:")
+    home_page = st.Page(f"{pages_dir}/home.py", title="Home", icon=":material/home:")
     stock_info_page = st.Page(
-        "pages/stock_info.py", title="Stock Information", icon=":material/query_stats:"
+        f"{pages_dir}/stock_info.py",
+        title="Stock Information",
+        icon=":material/query_stats:",
     )
     general_ai_analysis_page = st.Page(
-        "pages/general_ai_analysis.py",
+        f"{pages_dir}/general_ai_analysis.py",
         title="GENERAL AI ANALYSIS",
         icon=":material/science:",
     )
     ai_chat_page = st.Page(
-        "pages/ai_chat.py", title="AI Chat", icon=":material/chat_bubble:"
+        f"{pages_dir}/ai_chat.py", title="AI Chat", icon=":material/chat_bubble:"
     )
     about_page = st.Page(
-        "pages/about.py", title="ABOUT", icon=":material/format_quote:"
+        f"{pages_dir}/about.py", title="ABOUT", icon=":material/format_quote:"
     )
     pg = st.navigation(
         [home_page, stock_info_page, general_ai_analysis_page, ai_chat_page, about_page]
