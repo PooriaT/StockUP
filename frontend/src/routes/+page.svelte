2aff9eb7cf36f76f60aee4123b98ad6eb1d26408
PooriaT <pooria_taghdiri@hotmail.com>
2025-02-23 20:06:39 -0800

    static contents of home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import '../app.css';
+    let imgSrc = '/home_page_img_1.webp';
+</script>
+
+<div>
+    <h1>Welcome to StockUP</h1>
+
+    <img src={imgSrc} alt="StockUP" />
+
+    <p>
+        StockUP is an AI-driven stock market analysis tool designed to provide
+        insightful information on stocks. This application is a prototype and serves
+        as an educational platform to demonstrate how AI can be leveraged in financial
+        analysis.
+    </p>
+
+    <p>Features:</p>
+    <ul>
+        <li>
+            Easy-to-use interface for retrieving general stock information, historical data, and news
+            updates for any given stock symbol.
+        </li>
+        <li>
+            Utilizes the Gemini AI platform to generate detailed reports and trading recommendations based
+            on the latest available data.
+        </li>
+    </ul>
+</div>
