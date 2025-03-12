ef3134b411f73ccc0224afbde5dd2915dd40c2c9
Pooria <pooria_taghdiri@hotmail.com>
2025-03-11 22:05:49 -0700

    Revert "Migration"

--- app/setup/environment.py
+++
@@ -1,2 +0,0 @@
-TOP_COMPANIES_LIST = ["AAPL", "GOOGL", "MSFT", "TSLA", "AMZN", "NVDA", "META"]
-GEMINI_MODEL_NAME = "gemini-2.0-flash-exp"
---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+  import './about.css';
+</script>
+
+<div class="about-container">
+  <h1>About StockUP</h1>
+
+  <p>
+    Welcome to StockUP, an AI-driven stock market analysis tool designed to provide insightful
+    information on stocks. This application is a prototype and serves as an educational platform to
+    demonstrate how AI can be leveraged in financial analysis.
+  </p>
+
+  <p>Features:</p>
+  <ul>
+    <li>
+      Easy-to-use interface for retrieving general stock information, historical data, and news
+      updates for any given stock symbol.
+    </li>
+    <li>
+      Utilizes the Gemini AI platform to generate detailed reports and trading recommendations based
+      on the latest available data.
+    </li>
+  </ul>
+
+  <p>
+    DISCLAIMER: Please note that this is a prototype and should not be used for actual investment
+    decisions. All information is provided for educational purposes only, and the use of this
+    application is at your own risk.
+  </p>
+
+  <p>
+    Report Issues: <a href="https://github.com/PooriaT/StockUP/issues">GitHub Issue Tracker</a>
+  </p>
+
+  <p>
+    Project Link: <a href="https://github.com/PooriaT/StockUP">GitHub Repository</a>
+  </p>
+
+  <p>
+    Support the developer: <a href="https://buymeacoffee.com/pooria7">Buy me a Coffee</a>
+  </p>
+
+  <p>
+    Follow the builder on X: <a href="https://x.com/PooriaTaghdiri">Pooria's X</a>
+  </p>
+</div>
