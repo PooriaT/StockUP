1e64839c9f4e694d475e4ac7ac6e8a08f0762e60
PooriaT <pooria_taghdiri@hotmail.com>
2025-03-15 19:50:34 -0700

    fix:vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  preview: {
+    port: 10000,
+    host: true,
+    allowedHosts: ['stockupforall.onrender.com']
+  }
 });
